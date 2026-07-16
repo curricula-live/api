eeadb22c2ad0903caaa0ff0d0d31df0f4772f9db
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-07-16 14:36:29 +0300

    Wire custom graph administration site

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,19 @@
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from graph.admin import ConceptAdmin, GraphAdminSite, RelationAdmin
+from graph.models import Concept, Relation
 from graph.views import ConceptViewSet, RelationViewSet, graph_data
 
 router = DefaultRouter()
 router.register("concepts", ConceptViewSet)
 router.register("relations", RelationViewSet)
 
+graph_admin = GraphAdminSite(name="graph_admin")
+graph_admin.register(Concept, ConceptAdmin)
+graph_admin.register(Relation, RelationAdmin)
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", graph_admin.urls),
     path("api/", include(router.urls)),
     path("api/graph/", graph_data, name="graph-data"),
 ]
