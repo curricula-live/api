a771a34e60b377d7aa06c818d949766ddae4ab65
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-07-16 14:54:34 +0300

    Expose API health endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from graph.admin import ConceptAdmin, GraphAdminSite, RelationAdmin
 from graph.models import Concept, Relation
-from graph.views import ConceptViewSet, RelationViewSet, graph_data
+from graph.views import ConceptViewSet, RelationViewSet, graph_data, health
 
 router = DefaultRouter()
 router.register("concepts", ConceptViewSet)
@@ -15,5 +16,6 @@ graph_admin.register(Relation, RelationAdmin)
 urlpatterns = [
     path("admin/", graph_admin.urls),
     path("api/", include(router.urls)),
+    path("api/health/", health, name="health"),
     path("api/graph/", graph_data, name="graph-data"),
 ]
