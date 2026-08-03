18c90c470a54a9c4b6337396a575fd2a7ca74489
db@duet5i.wsl.arch <dbxdrgsl@gmail.com>
2026-08-03 07:18:41 +0300

    fixed indentation and removed commented out typo

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,12 +1,11 @@
 from django.urls import reverse
 
+
 def test_health_endpoint(client):
-    # respsonse = client.get("/health/")
     response = client.get(reverse("health"))
 
     assert response.status_code == 200
     assert response.json() == {
-            "status": "ok",
-            "service": "curricula.live api",
-            }   
-
+        "status": "ok",
+        "service": "curricula.live api",
+    }
