aa061020aa1b143b3a628251b0630d3e5b37ecd6
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-22 23:19:27 +0300

    feat: route concept read endpoints

--- config/urls.py
+++ config/urls.py
@@ -1,26 +1,13 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""URL configuration for the curricula.live API."""
 
 from django.contrib import admin
 from django.urls import path
 
-from core.views import health
+from core.views import concept_detail, concept_list, health
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", health, name="health"),
+    path("api/concepts/", concept_list, name="concept-list"),
+    path("api/concepts/<str:slug>/", concept_detail, name="concept-detail"),
 ]
