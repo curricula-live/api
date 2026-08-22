bf80ee2e8d529fc786d22e81a9843f9ab0131b3d
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-22 23:19:21 +0300

    feat: add concept read views

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
+from core.models import Concept
 
 
 def health(request):
@@ -11,4 +13,11 @@ def health(request):
     )
 
 
-# Create your views here.
+def concept_list(request):
+    concepts = Concept.objects.order_by("slug").values("slug")
+    return JsonResponse({"results": list(concepts)})
+
+
+def concept_detail(request, slug):
+    concept = get_object_or_404(Concept, slug=slug)
+    return JsonResponse({"slug": concept.slug})
