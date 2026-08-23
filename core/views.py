396e47f815ede107de00eecb1dfa965c08bc8c3f
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-23 10:35:02 +0300

    feat: add relation read views

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_GET
 
-from core.models import Concept
+from core.models import Concept, Relation
 
 
 def health(request):
@@ -24,3 +24,35 @@ def concept_list(request):
 def concept_detail(request, slug):
     concept = get_object_or_404(Concept, slug=slug)
     return JsonResponse({"slug": concept.slug})
+
+
+def _relation_payload(relation):
+    return {
+        "id": str(relation.id),
+        "source": relation.source_id,
+        "type": relation.type_id,
+        "target": relation.target_id,
+    }
+
+
+@require_GET
+def relation_list(request):
+    relations = Relation.objects.all()
+
+    for parameter, field in (
+        ("source", "source_id"),
+        ("type", "type_id"),
+        ("target", "target_id"),
+    ):
+        value = request.GET.get(parameter)
+        if value is not None:
+            relations = relations.filter(**{field: value})
+
+    relations = relations.order_by("source_id", "type_id", "target_id", "id")
+    return JsonResponse({"results": [_relation_payload(relation) for relation in relations]})
+
+
+@require_GET
+def relation_detail(request, relation_id):
+    relation = get_object_or_404(Relation, id=relation_id)
+    return JsonResponse(_relation_payload(relation))
