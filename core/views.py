88123e7bafe991816b3200be852b7ec5a177444a
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-23 10:19:52 +0300

    Restrict concept API endpoints to GET

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_GET
 
 from core.models import Concept
 
@@ -13,11 +14,13 @@ def health(request):
     )
 
 
+@require_GET
 def concept_list(request):
     concepts = Concept.objects.order_by("slug").values("slug")
     return JsonResponse({"results": list(concepts)})
 
 
+@require_GET
 def concept_detail(request, slug):
     concept = get_object_or_404(Concept, slug=slug)
     return JsonResponse({"slug": concept.slug})
