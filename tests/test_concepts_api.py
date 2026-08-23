4f3f351ced9f1fe3bac1cdb3954c251374f349fb
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-23 10:19:58 +0300

    Test concept API method restrictions

--- tests/test_concepts_api.py
+++ tests/test_concepts_api.py
@@ -29,3 +29,21 @@ def test_concept_detail_returns_404_for_unknown_slug(client, curriculum_graph):
     response = client.get(reverse("concept-detail", kwargs={"slug": "missing"}))
 
     assert response.status_code == 404
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("method", ["post", "put", "patch", "delete"])
+def test_concept_list_rejects_unsupported_methods(client, method):
+    response = getattr(client, method)(reverse("concept-list"))
+
+    assert response.status_code == 405
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("method", ["post", "put", "patch", "delete"])
+def test_concept_detail_rejects_unsupported_methods(client, curriculum_graph, method):
+    response = getattr(client, method)(
+        reverse("concept-detail", kwargs={"slug": "array"})
+    )
+
+    assert response.status_code == 405
