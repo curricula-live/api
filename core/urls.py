b3f43d0f7d8356e8dee8b5a6f7762940b86697af
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-23 10:35:07 +0300

    feat: route relation read endpoints

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from core.views import concept_detail, concept_list
+from core.views import concept_detail, concept_list, relation_detail, relation_list
 
 urlpatterns = [
     path("concepts/", concept_list, name="concept-list"),
     path("concepts/<str:slug>/", concept_detail, name="concept-detail"),
+    path("relations/", relation_list, name="relation-list"),
+    path("relations/<uuid:relation_id>/", relation_detail, name="relation-detail"),
 ]
