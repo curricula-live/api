ec59f3ac3c746daf77b3feec7d6392cac0cf90b9
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-23 12:57:15 +0300

    Route concept neighbourhood endpoint

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from core.views import (
     concept_detail,
     concept_list,
+    concept_neighborhood,
     relation_detail,
     relation_list,
     relation_type_list,
@@ -11,6 +12,11 @@ from core.views import (
 urlpatterns = [
     path("concepts/", concept_list, name="concept-list"),
     path("concepts/<str:slug>/", concept_detail, name="concept-detail"),
+    path(
+        "concepts/<str:slug>/neighborhood/",
+        concept_neighborhood,
+        name="concept-neighborhood",
+    ),
     path("relations/", relation_list, name="relation-list"),
     path("relations/<uuid:relation_id>/", relation_detail, name="relation-detail"),
     path("relation-types/", relation_type_list, name="relation-type-list"),
