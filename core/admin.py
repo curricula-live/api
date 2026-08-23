bd01cef01ba60457dfafc5584dfb65d19b47a9ae
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-23 14:17:12 +0300

    Add Django graph administration interface (#30)
    * Add Django graph administration
    
    * Test graph admin configuration
    
    * Protect graph primary keys in admin
    
    * Test immutable graph primary keys in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Concept, Relation, RelationType
+
+
+class ImmutablePrimaryKeyAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None:
+            return ()
+        return (self.model._meta.pk.name,)
+
+
+@admin.register(Concept)
+class ConceptAdmin(ImmutablePrimaryKeyAdmin):
+    search_fields = ("slug",)
+    ordering = ("slug",)
+
+
+@admin.register(RelationType)
+class RelationTypeAdmin(ImmutablePrimaryKeyAdmin):
+    search_fields = ("slug",)
+    ordering = ("slug",)
+
+
+@admin.register(Relation)
+class RelationAdmin(ImmutablePrimaryKeyAdmin):
+    list_display = ("source", "type", "target")
+    list_filter = ("type",)
+    search_fields = ("source__slug", "target__slug", "type__slug")
+    ordering = ("source_id", "type_id", "target_id")
+    autocomplete_fields = ("source", "type", "target")
