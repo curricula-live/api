74ec71837f4b48b6cf0831c2c94ccfc0143a03ed
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-08-23 14:08:17 +0300

    Add Django graph administration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Concept, Relation, RelationType
+
+
+@admin.register(Concept)
+class ConceptAdmin(admin.ModelAdmin):
+    search_fields = ("slug",)
+    ordering = ("slug",)
+
+
+@admin.register(RelationType)
+class RelationTypeAdmin(admin.ModelAdmin):
+    search_fields = ("slug",)
+    ordering = ("slug",)
+
+
+@admin.register(Relation)
+class RelationAdmin(admin.ModelAdmin):
+    list_display = ("source", "type", "target")
+    list_filter = ("type",)
+    search_fields = ("source__slug", "target__slug", "type__slug")
+    ordering = ("source_id", "type_id", "target_id")
+    autocomplete_fields = ("source", "type", "target")
